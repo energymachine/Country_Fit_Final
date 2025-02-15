f7c3051d8be0f04aa7a4a6cdf324e06748d62032
energymachine <154980305+energymachine@users.noreply.github.com>
2025-02-15 11:07:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -45,4 +45,12 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById("menu-toggle");
+    const navMenu = document.getElementById("nav-menu");
+
+    menuToggle.addEventListener("click", function () {
+        navMenu.classList.toggle("show");
+    });
+});
 
